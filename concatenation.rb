89b195b1a0e96a5b0c59d3819649b1f2661eba77
Fernando Moreira <hi@nandomoreira.me>
2014-12-31 13:59:22 -0200

    Concatenation and operations

---
+++ concatenation.rb
@@ -0,0 +1,11 @@
+# Concatenation
+
+puts "Hello" + " " + "World!"
+
+puts "Hello" + " " + "World! " * 5
+
+puts "10" + "5"
+
+puts "10" * 5
+
+puts "K" * 100
---
+++ operations.rb
@@ -0,0 +1,10 @@
+# Operacoes Aritmeticas
+
+puts 1+1
+puts 3-2
+puts 6*3
+puts 30/2
+
+# com parenteses
+puts 10+10/2
+puts (10+10)/2
