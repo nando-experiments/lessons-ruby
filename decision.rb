24897b53134a33ca900908da6d502007801be813
Fernando Moreira <hi@nandomoreira.me>
2014-12-31 14:55:26 -0200

    Decision :v:

---
+++ decision.rb
@@ -0,0 +1,22 @@
+﻿# Estrutura de decisão :)
+
+puts "==="
+
+puts "Digite seu nome"
+nome = gets.chomp
+
+puts "==="
+
+puts "Digite seu Sexo (M ou F)"
+sexo = gets.chomp.upcase
+
+puts "==="
+puts "Seu nome é: " + nome
+
+if sexo == "M"
+    puts "Sexo: Masculino."
+elsif sexo == "F"
+    puts "Sexo: Feminino."
+else
+    puts "Não foi possível verificar!"
+end
