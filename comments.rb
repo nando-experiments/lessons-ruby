f961f1fe8ed9dc991a09d5abc519d4119c9fad48
Fernando Moreira <hi@nandomoreira.me>
2014-12-31 14:01:43 -0200

    comments

---
+++ comments.rb
@@ -0,0 +1,13 @@
+# Comentario de 1 linha
+puts "Hello World!"
+
+=begin
+
+Comentario de multiplas linhas
+
+puts "Hello World!"
+puts "Lorem ipsum dolor sit amet, consectetuer adipiscing elit."
+puts "Nam cursus. Morbi ut mi. Nullam enim leo, egestas id"
+puts "condimentum at, laoreet mattis, massa."
+
+=end
