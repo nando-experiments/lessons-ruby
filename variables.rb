5fb50c35ce72473f4401a2b6e1963a6a42292ffe
Fernando Moreira <hi@nandomoreira.me>
2014-12-31 14:19:19 -0200

    converter variaveis :blush:

--- variables.rb
+++ variables.rb
@@ -3,3 +3,31 @@
 var1 = "World!"
 
 puts "Hello " + var1
+
+# converter variaveis
+x = "10"
+y = 5
+
+puts y + x.to_i
+
+#######
+
+puts "----"
+
+#######
+
+puts "O resultado da soma de x e y e: " + (y + x.to_i).to_s
+
+#######
+
+puts "----"
+
+nota1 = 7.4
+nota2 = 8
+nota3 = 6.3
+nota4 = 6
+soma = (nota1 + nota2 + nota3 + nota4)
+media = soma / 4
+
+puts "A soma das notas e: " + soma.to_s
+puts "A sua media e: " + media.to_s
