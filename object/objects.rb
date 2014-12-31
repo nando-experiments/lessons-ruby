8796d76fcee795ff9bbfaee3f4fc0cd4cee5db7b
Fernando Moreira <hi@nandomoreira.me>
2014-12-31 15:46:26 -0200

    Objects :kissing_heart:

---
+++ object/objects.rb
@@ -0,0 +1,22 @@
+﻿# tudo é Objeto no Ruby
+
+puts "object".upcase
+
+puts "OBJECT".downcase
+
+puts "Object".reverse
+
+puts "Object".length
+
+def method_test
+    "Ola mundo!"
+end
+
+puts method_test
+puts method_test.length
+
+def total_values(val1, val2, val3)
+    (val1 + val2 + val3)
+end
+
+puts total_values(21, 13, 4)
