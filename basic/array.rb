87635acdfef5ed7ee1271cf54ccb9757748daef3
Fernando Moreira <hi@nandomoreira.me>
2014-12-31 15:13:01 -0200

    basic directory and arrays

---
+++ basic/array.rb
@@ -0,0 +1,14 @@
+﻿notas = [10, 5, 6, 8.5]
+
+#puts notas[0]
+#puts notas[3]
+
+notas.each do |nota|
+    puts nota
+end
+
+nomes = ["Ana", "Maria", "João", "Bruno", "Paulo"]
+
+nomes.each do |nome|
+    puts nome
+end
---
+++ basic/loop-while.rb
@@ -0,0 +1,27 @@
+﻿# loop while
+
+puts "==="
+
+puts "Digite seu nome"
+nome = gets.chomp
+
+puts "==="
+
+puts "Digite seu Sexo (M ou F)"
+sexo = gets.chomp.upcase
+
+# se o que for digitado for diferente de F ou M
+# o loop vai continuar perguntando o sexo :P
+while sexo != "M" && sexo != "F"
+    puts "Digite seu Sexo (M ou F)"
+    sexo = gets.chomp.upcase
+end
+
+puts "==="
+puts "Seu nome é: " + nome
+
+if sexo == "M"
+    puts "Sexo: Masculino."
+elsif sexo == "F"
+    puts "Sexo: Feminino."
+end
